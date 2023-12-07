04e1172a949505711e8069ea7514c96620f61991
Yash Chavan <stepron.ychavan@gmail.com>
2023-12-07 17:16:42 +0530

    addded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+
 import Footer from "./components/layouts/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -20,67 +21,88 @@ import Header from "./components/layouts/Header";
 import { Helmet } from "react-helmet";
 
 function App() {
+  const location = useLocation();
+
+  function getCurrentPageTitle() {
+    const path = location.pathname;
+    switch (path) {
+      case "/":
+        return "Home";
+      case "/about":
+        return "About Us";
+      case "/safety":
+        return "Safety";
+      case "/services/building-maintenance":
+        return "Building Maintenance";
+      case "/services/24-hour-emergency-service":
+        return "Emergency Service";
+      case "/services/commercial-roofing-maintenance-repairs":
+        return "Commercial Roofing";
+      case "/services/tenant-and-mechanical-upgrades":
+        return "Roof Top Repair";
+      case "/services/commercial-re-roofing":
+        return "Commercial Re-Roofing";
+      case "/services/commercial-wall-systems":
+        return "Commercial Wall Systems";
+      case "/TrustThermalSystems":
+        return "Trust Thermal Systems";
+      case "/FlatRoofRepairAirdrie":
+        return "Flat Roof Repair in Airdrie";
+      case "/CommercialCladdingRepairCalgary":
+        return "Commercial Cladding Repair in Calgary";
+      case "/BuildingEnvelopeRepairCalgary":
+        return "Building Envelope Repair in Calgary";
+      case "/contact":
+        return "Contact";
+      default:
+        return "Thermal Systems";
+    }
+  }
   return (
     <div>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/about" element={<About />}></Route>
-      <Route path="/safety" element={<Safety />}></Route>     
-      <Route path="/services/building-maintenance" element={<BuildingMaintenance />}></Route>
-      <Route path="/services/24-hour-emergency-service" element={<EmergencyService />}></Route>
-      <Route path="/services/commercial-roofing-maintenance-repairs" element={<CommercialRoofing />}></Route>
-      <Route path="/services/tenant-and-mechanical-upgrades" element={<RoofTopRepair />}></Route>
-      <Route path="/services/commercial-re-roofing" element={<CommercialReRoofing />}></Route>
-      <Route path="/services/commercial-wall-systems" element={<CommercialWall />}></Route>
-      <Route path="/TrustThermalSystems" element={<TrustThermalSystems />}></Route>
-      <Route path="/FlatRoofRepairAirdrie" element={<FlatRoofRepairAirdrie />}></Route>
-      <Route path="/CommercialCladdingRepairCalgary" element={<CommercialCladdingRepairCalgary />}></Route>
-      <Route path="/BuildingEnvelopeRepairCalgary" element={<BuildingEnvelopeRepairCalgary />}></Route>
-      <Route path="/contact" element={<Contact />}></Route>
-    </Routes>
-    <Helmet>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/safety" element={<Safety />}></Route>
+        <Route
+          path="/services/building-maintenance"
+          element={<BuildingMaintenance />}></Route>
+        <Route
+          path="/services/24-hour-emergency-service"
+          element={<EmergencyService />}></Route>
+        <Route
+          path="/services/commercial-roofing-maintenance-repairs"
+          element={<CommercialRoofing />}></Route>
+        <Route
+          path="/services/tenant-and-mechanical-upgrades"
+          element={<RoofTopRepair />}></Route>
+        <Route
+          path="/services/commercial-re-roofing"
+          element={<CommercialReRoofing />}></Route>
+        <Route
+          path="/services/commercial-wall-systems"
+          element={<CommercialWall />}></Route>
+        <Route
+          path="/TrustThermalSystems"
+          element={<TrustThermalSystems />}></Route>
+        <Route
+          path="/FlatRoofRepairAirdrie"
+          element={<FlatRoofRepairAirdrie />}></Route>
+        <Route
+          path="/CommercialCladdingRepairCalgary"
+          element={<CommercialCladdingRepairCalgary />}></Route>
+        <Route
+          path="/BuildingEnvelopeRepairCalgary"
+          element={<BuildingEnvelopeRepairCalgary />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+      <Helmet>
         <title>{getCurrentPageTitle()} | Thermal Systems</title>
       </Helmet>
-    <Footer />
+      <Footer />
     </div>
   );
-}
-
-function getCurrentPageTitle() {
-  const path = window.location.pathname;
-  switch (path) {
-    case "/":
-      return "Home";
-    case "/about":
-      return "About Us";
-    case "/safety":
-      return "Safety";
-    case "/services/building-maintenance":
-      return "Building Maintenance";
-    case "/services/24-hour-emergency-service":
-      return "Emergency Service";
-    case "/services/commercial-roofing-maintenance-repairs":
-      return "Commercial Roofing";
-    case "/services/tenant-and-mechanical-upgrades":
-      return "Roof Top Repair";
-    case "/services/commercial-re-roofing":
-      return "Commercial Re-Roofing";
-    case "/services/commercial-wall-systems":
-      return "Commercial Wall Systems";
-    case "/TrustThermalSystems":
-      return "Trust Thermal Systems";
-    case "/FlatRoofRepairAirdrie":
-      return "Flat Roof Repair in Airdrie";
-    case "/CommercialCladdingRepairCalgary":
-      return "Commercial Cladding Repair in Calgary";
-    case "/BuildingEnvelopeRepairCalgary":
-      return "Building Envelope Repair in Calgary";
-    case "/contact":
-      return "Contact";
-    default:
-      return "Thermal Systems";
-  }
 }
 
 export default App;
