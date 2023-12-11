73d4ccc3deb7ac2283fa5beb118865672c79c35c
Yash Chavan <stepron.ychavan@gmail.com>
2023-12-11 10:07:57 +0530

    goggle

--- src/index.js
+++ src/index.js
@@ -22,7 +22,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-LWGJTXBC9P");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
