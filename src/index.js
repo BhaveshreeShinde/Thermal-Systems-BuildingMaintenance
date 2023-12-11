6262fb4e708ae0981fd1589a19169a9969c4beb3
BhaveshreeShinde <sbhaveshree219@gmail.com>
2023-12-11 11:23:07 +0530

    updated code

--- src/index.js
+++ src/index.js
@@ -1,62 +1,9 @@
-// // import React from 'react';
-// // import ReactDOM from 'react-dom/client';
-// // import './index.css';
-// // import App from './App';
-// // import reportWebVitals from './reportWebVitals';
-
-// // const root = ReactDOM.createRoot(document.getElementById('root'));
-// // root.render(
-// //   <React.StrictMode>
-// //     <App />
-// //   </React.StrictMode>
-// // );
-
-// // // If you want to start measuring performance in your app, pass a function
-// // // to log results (for example: reportWebVitals(console.log))
-// // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// // reportWebVitals();
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-// import { BrowserRouter } from "react-router-dom";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// );
-
-// reportWebVitals();
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-// import { hydrate, render } from "react-dom";
 import ReactGA from "react-ga4";
 
 ReactGA.initialize("G-LWGJTXBC9P");
@@ -65,10 +12,16 @@ ReactGA.send({
   page: window.location.pathname,
 });
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
+const root = document.getElementById("root");
+const reactRoot = ReactDOM.createRoot(root);
+
+if (root.hasChildNodes()) {
+  reactRoot.hydrate(<BrowserRouter>
     <App />
-  </BrowserRouter>
-);
+  </BrowserRouter>);
+} else {
+  reactRoot.render(<BrowserRouter>
+    <App />
+  </BrowserRouter>);
+}
 reportWebVitals();
