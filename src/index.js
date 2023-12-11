9e3811a3d8fa409ff024332751cfc847da173459
Yash Chavan <stepron.ychavan@gmail.com>
2023-12-11 11:01:12 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+// // import React from 'react';
+// // import ReactDOM from 'react-dom/client';
+// // import './index.css';
+// // import App from './App';
+// // import reportWebVitals from './reportWebVitals';
+
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+// // root.render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>
+// // );
+
+// // // If you want to start measuring performance in your app, pass a function
+// // // to log results (for example: reportWebVitals(console.log))
+// // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// // reportWebVitals();
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import { BrowserRouter } from "react-router-dom";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
+
+// reportWebVitals();
+
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import './index.css';
@@ -22,27 +56,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { hydrate, render } from "react-dom";
+// import { hydrate, render } from "react-dom";
 import ReactGA from "react-ga4";
-
 
 ReactGA.initialize("G-LWGJTXBC9P");
 ReactGA.send({
   hitType: "pageview",
   page: window.location.pathname,
 });
-const MyApp = (
-  <BrowserRouter>
-  <App />
-</BrowserRouter>
-);
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-if (root.hasChildNodes()) {
-  hydrate(MyApp, root);
-} else {
-  render(MyApp, root);
-}
-
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 reportWebVitals();
